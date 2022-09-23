f90a968380bde8d79d7b66600d6212c68b755346
LucasDegoumois <lucasleoneldeg@gmail.com>
2022-09-23 07:14:37 -0300

    paglinks

--- Materias/Quimica General/general.js
+++ Materias/Quimica General/general.js
@@ -8,16 +8,6 @@ function mostrar_menu(){
 }
 
 
-//Esto es una mini animacion pedorra de los iconos del banner uwu
-
-window.onscroll = function () {
-    var posicion = window.pageYOffset || document.documentElement.scrollTop;
-    var elemento1 = document.getElementById("icon_heart");
-    var elemento2 = document.getElementById("icon_fire");
-    elemento1.style.bottom = posicion * 0.23 + "px";
-    elemento2.style.top = posicion * 0.5 + "px";
-}
-
 
 document.getElementById("icon-search").addEventListener("click", mostrar_buscador);
 document.getElementById("cover-ctn-search").addEventListener("click", ocultar_buscador);
--- Materias/Quimica Inorganica/Azufre/azufre.js
+++ Materias/Quimica Inorganica/Azufre/azufre.js
@@ -5,18 +5,9 @@ document.getElementById("icon_menu").addEventListener("click", mostrar_menu);
 function mostrar_menu(){
 
     document.querySelector(".menu").classList.toggle("mostrar_menu");
+    
 }
 
-
-//Esto es una mini animacion pedorra de los iconos del banner uwu
-
-window.onscroll = function () {
-    var posicion = window.pageYOffset || document.documentElement.scrollTop;
-    var elemento1 = document.getElementById("icon_heart");
-    var elemento2 = document.getElementById("icon_fire");
-    elemento1.style.bottom = posicion * 0.23 + "px";
-    elemento2.style.top = posicion * 0.5 + "px";
-}
 
 
 document.getElementById("icon-search").addEventListener("click", mostrar_buscador);
